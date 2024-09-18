a3ddbd4ea221b839d68443ab91ed755252017084
overtrue <anzhengchao@gmail.com>
2024-09-19 00:35:00 +0800

    wip

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ async function generateErrorReport(driver) {
   const form = new FormData()
   form.append('file', fs.createReadStream('error.png'))
 
-  return new Promise((resolve, reject) => {
+  const response = await new Promise((resolve, reject) => {
     form.submit('https://imagebin.ca/upload.php', (err, res) => {
       if (err) return reject(err)
       res.resume()
@@ -85,251 +85,254 @@ async function generateErrorReport(driver) {
       res.on('end', () => resolve())
     })
   })
-}
 
-// proxyUrl: http://username:password@host:port
-// proxyUrl: socks://username:password@host:port
-async function getProxyUrl(proxyUrl, schema) {
-  // ensure starts with http:// or socks://
-  if (!proxyUrl.startsWith(`${schema}://`)) {
-    proxyUrl = `${schema}://${proxyUrl}`
+  console.log('-> Error report uploaded:', response)
+
+  // proxyUrl: http://username:password@host:port
+  // proxyUrl: socks://username:password@host:port
+  async function getProxyUrl(proxyUrl, schema) {
+    // ensure starts with http:// or socks://
+    if (!proxyUrl.startsWith(`${schema}://`)) {
+      proxyUrl = `${schema}://${proxyUrl}`
+    }
+
+    try {
+      console.log('-> Anonymizing proxy URL:', proxyUrl)
+
+      const anonymizedProxy = await proxyChain.anonymizeProxy(proxyUrl)
+      // parse the anonymized proxy URL
+      const parsedUrl = new URL(anonymizedProxy)
+
+      // extract the host and port
+      const proxyHost = parsedUrl.hostname
+      const proxyPort = parsedUrl.port
+      const newProxyUrl = `${proxyHost}:${proxyPort}`
+
+      // construct the new proxy string
+      console.log('-> Anonymized proxy:', `${newProxyUrl}`)
+
+      return newProxyUrl
+    } catch (error) {
+      console.error(`-> Error anonymizing proxy URL (${proxyUrl}):`, error)
+      return proxyUrl
+    }
   }
 
-  try {
-    console.log('-> Anonymizing proxy URL:', proxyUrl)
+  async function getProxyIpInfo(proxyUrl) {
+    // const url = `https://httpbin.org/ip`
+    const url = 'http://myip.ipip.net'
 
-    const anonymizedProxy = await proxyChain.anonymizeProxy(proxyUrl)
-    // parse the anonymized proxy URL
-    const parsedUrl = new URL(anonymizedProxy)
+    console.log('-> Getting proxy IP info:', proxyUrl)
 
-    // extract the host and port
-    const proxyHost = parsedUrl.hostname
-    const proxyPort = parsedUrl.port
-    const newProxyUrl = `${proxyHost}:${proxyPort}`
+    const options = new chrome.Options()
 
-    // construct the new proxy string
-    console.log('-> Anonymized proxy:', `${newProxyUrl}`)
+    options.addArguments(`user-agent=${USER_AGENT}`)
+    options.addArguments('--headless')
+    options.addArguments('--disable-dev-shm-usage')
+    options.addArguments('--disable-gpu')
+    options.addArguments('--no-sandbox')
 
-    return newProxyUrl
-  } catch (error) {
-    console.error(`-> Error anonymizing proxy URL (${proxyUrl}):`, error)
-    return proxyUrl
-  }
-}
+    options.setProxy(proxy.manual({
+      http: proxyUrl,
+      https: proxyUrl,
+    }))
 
-async function getProxyIpInfo(proxyUrl) {
-  // const url = `https://httpbin.org/ip`
-  const url = 'http://myip.ipip.net'
-
-  console.log('-> Getting proxy IP info:', proxyUrl)
-
-  const options = new chrome.Options()
-
-  options.addArguments(`user-agent=${USER_AGENT}`)
-  options.addArguments('--headless')
-  options.addArguments('--disable-dev-shm-usage')
-  options.addArguments('--disable-gpu')
-  options.addArguments('--no-sandbox')
-
-  options.setProxy(proxy.manual({
-    http: proxyUrl,
-    https: proxyUrl,
-  }))
-
-  const driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(options)
-    .build()
-
-  try {
-    console.log('-> Proxy IP info started!')
-
-    await driver.get(url)
-    await driver.sleep(5000)
-
-    const pageText = await driver.findElement(By.css('body')).getText()
-    console.log('-> Proxy IP info:', pageText)
-  } catch (error) {
-    console.error('-> Error getting proxy IP info:', error)
-  } finally {
-    await driver.quit()
-    console.log('-> Proxy IP info done!')
-  }
-}
-
-(async () => {
-  console.log('-> Downloading extension...')
-  await downloadExtension(extensionId)
-  console.log('-> Downloaded! Installing extension and driver manager...')
-
-  const options = new chrome.Options()
-
-  if (PROXY) {
-    const httpProxyUrl = await getProxyUrl(PROXY, 'http')
-    // const wssProxyUrl = await getProxyUrl(PROXY.replace('http://', 'ws://').replace(1337, 1338), 'socks')
-    const wssProxyUrl = await getProxyUrl(PROXY.replace('http://', 'ws://'), 'socks5')
-
-    // options.setProxy(proxy.manual({
-    //   http: httpProxyUrl,
-    //   https: httpProxyUrl,
-    // }))
-
-    options.setProxy(proxy.socks(wssProxyUrl, 5))
-
-    console.log('-> Using proxy http:', httpProxyUrl)
-    console.log('-> Using proxy ws:', wssProxyUrl)
-
-    // await getProxyIpInfo(httpProxyUrl)
-  } else {
-    console.log('-> No proxy set!')
-  }
-
-  options.addArguments(`user-agent=${USER_AGENT}`)
-  options.addArguments('--headless')
-  options.addArguments('--disable-dev-shm-usage')
-  options.addArguments('--no-sandbox')
-  options.addArguments('--disable-gpu')
-  options.addArguments('--disable-ipv6')
-  options.addArguments('--disable-features=InsecureDoNotUseIpv6')
-  options.addExtensions(path.resolve(__dirname, EXTENSION_FILENAME))
-
-  let driver
-  try {
-    driver = await new Builder()
+    const driver = await new Builder()
       .forBrowser('chrome')
       .setChromeOptions(options)
       .build()
 
-    console.log('-> Started! Logging in...')
-    await driver.get('https://app.gradient.network/')
-
-    const emailInput = By.css('[placeholder="Enter Email"]')
-    const passwordInput = By.css('[type="password"]')
-    const loginButton = By.css('button')
-
-    await driver.wait(until.elementLocated(emailInput), 15000)
-    await driver.wait(until.elementLocated(passwordInput), 15000)
-    await driver.wait(until.elementLocated(loginButton), 15000)
-
-    await driver.findElement(emailInput).sendKeys(USER)
-    await driver.findElement(passwordInput).sendKeys(PASSWORD)
-    await driver.findElement(loginButton).click()
-
-    await driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Copy Referral Link")]')), 30000)
-
-    console.log('-> Logged in! Waiting for open extension...')
-
-    // 截图登录状态
-    takeScreenshot(driver, 'login.png')
-
-    await driver.get(`chrome-extension://${extensionId}/popup.html`)
-
-    // 直到找到 "Status" 文本的 div 元素
-    await driver.wait(until.elementLocated(By.xpath('//div[contains(text(), "Status")]')), 30000)
-
-    await driver.sleep(5000)
-
-    console.log('-> Extension loaded!')
-
-    // if there is a page with a button "I got it", click it
     try {
-      const gotItButton = await driver.findElement(By.xpath('//button[contains(text(), "I got it")]'))
-      await gotItButton.click()
-      console.log('-> "I got it" button clicked!')
+      console.log('-> Proxy IP info started!')
+
+      await driver.get(url)
+      await driver.sleep(5000)
+
+      const pageText = await driver.findElement(By.css('body')).getText()
+      console.log('-> Proxy IP info:', pageText)
     } catch (error) {
-      // save rendered dom to file
-      const dom = await driver.findElement(By.css('html')).getAttribute('outerHTML')
-      fs.writeFileSync('dom.html', dom)
-      console.error('-> No "I got it" button found!')
-    }
-
-    // if found a div include text "Sorry, Gradient is not yet available in your region. ", then exit
-    try {
-      const notAvailable = await driver.findElement(By.xpath('//*[contains(text(), "Sorry, Gradient is not yet available in your region.")]'))
-      console.log('-> Sorry, Gradient is not yet available in your region. ')
+      console.error('-> Error getting proxy IP info:', error)
+    } finally {
       await driver.quit()
-      process.exit(1)
-    } catch (error) {
-      console.log('-> Gradient is available in your region. ')
-    }
-
-    await driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Today\'s Taps")]')), 30000)
-
-    const statusBadge = await driver.findElement(By.css('.absolute.mt-3.right-0.z-10')).getText()
-
-    const dom = await driver.findElement(By.css('html')).getAttribute('outerHTML')
-    fs.writeFileSync('dom.html', dom)
-
-    await takeScreenshot(driver, 'status.png')
-
-    console.log('-> Status:', statusBadge)
-
-    if (statusBadge.includes('Disconnected')) {
-      console.log('-> Failed to connect! may be the proxy has been banned \nGenerating error report...')
-      await generateErrorReport(driver)
-      await driver.quit()
-      process.exit(1)
-    }
-
-    console.log('-> Connected! Starting API...')
-
-    // 截图链接状态
-    takeScreenshot(driver, 'connected.png')
-
-    const app = express()
-
-    app.get('/', async (req, res) => {
-      try {
-        // <div class="absolute mt-3 right-0 z-10">
-        const supportStatus = await driver.findElement(By.css('.absolute.mt-3.right-0.z-10')).getText()
-
-        // <div class="Helveticae w-1/2 flex justify-center items-center text-[14px] select-none cursor-pointer z-20 text-white">Reward</div>
-        const statusTab = await driver.findElement(By.xpath('//div[contains(text(), "Status")]'))
-        const rewardTab = await driver.findElement(By.xpath('//div[contains(text(), "Reward")]'))
-
-        // todaysTaps:
-        // <div class="Helveticae font-bold flex justify-center items-center select-none mt-[2px]" style="font-size: 24px;">0</div>
-        const todaysTaps = await driver.findElement(By.css('.mr-[8px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
-        const tadaysUptime = await driver.findElement(By.css('.mr-[4px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
-
-        // click on rewardTab tab
-        await rewardTab.click()
-
-        // todayReward:
-        // <div class="Helveticae font-bold flex justify-center items-center select-none mt-[2px]" style="font-size: 24px;">0</div>
-        const todayReward = await driver.findElement(By.css('.mr-[8px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
-        const seasonReward = await driver.findElement(By.css('.mr-[4px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
-
-        const connected = badgeTexts.some(text => text.includes('Connected'))
-
-        res.json({
-          connected,
-          supportStatus: supportStatus,
-          today_taps: todaysTaps,
-          today_uptime: tadaysUptime,
-          today_reward: todayReward,
-          season_reward: seasonReward,
-        })
-      } catch (error) {
-        console.error(error)
-        await generateErrorReport(driver)
-        res.status(500).send('Internal Server Error')
-      }
-    })
-
-    app.listen(8090, '0.0.0.0', () => {
-      console.log('-> API server running on port 80 http://localhost:8090')
-    })
-
-    console.log('-> API started! Waiting for requests...')
-  } catch (error) {
-    console.error('Error occurred:', error)
-    if (driver) {
-      await generateErrorReport(driver)
-    }
-  } finally {
-    if (driver) {
-      await driver.quit()
+      console.log('-> Proxy IP info done!')
     }
   }
-})()
+
+  (async () => {
+    console.log('-> Downloading extension...')
+    await downloadExtension(extensionId)
+    console.log('-> Downloaded! Installing extension and driver manager...')
+
+    const options = new chrome.Options()
+
+    if (PROXY) {
+      const httpProxyUrl = await getProxyUrl(PROXY, 'http')
+      // const wssProxyUrl = await getProxyUrl(PROXY.replace('http://', 'ws://').replace(1337, 1338), 'socks')
+      const wssProxyUrl = await getProxyUrl(PROXY.replace('http://', 'ws://'), 'socks5')
+
+      options.setProxy(proxy.manual({
+        http: httpProxyUrl,
+        https: httpProxyUrl,
+      }))
+
+      // options.setProxy(proxy.socks(wssProxyUrl, 5))
+      options.addArguments(`--proxy-server=socks5://${wssProxyUrl}`)
+
+      console.log('-> Using proxy http:', httpProxyUrl)
+      console.log('-> Using proxy ws:', wssProxyUrl)
+
+      // await getProxyIpInfo(httpProxyUrl)
+    } else {
+      console.log('-> No proxy set!')
+    }
+
+    options.addArguments(`user-agent=${USER_AGENT}`)
+    options.addArguments('--headless')
+    options.addArguments('--disable-dev-shm-usage')
+    options.addArguments('--no-sandbox')
+    options.addArguments('--disable-gpu')
+    options.addArguments('--disable-ipv6')
+    options.addArguments('--remote-allow-origins=*')
+    options.addArguments('--disable-features=InsecureDoNotUseIpv6')
+    options.addExtensions(path.resolve(__dirname, EXTENSION_FILENAME))
+
+    let driver
+    try {
+      driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build()
+
+      console.log('-> Started! Logging in...')
+      await driver.get('https://app.gradient.network/')
+
+      const emailInput = By.css('[placeholder="Enter Email"]')
+      const passwordInput = By.css('[type="password"]')
+      const loginButton = By.css('button')
+
+      await driver.wait(until.elementLocated(emailInput), 15000)
+      await driver.wait(until.elementLocated(passwordInput), 15000)
+      await driver.wait(until.elementLocated(loginButton), 15000)
+
+      await driver.findElement(emailInput).sendKeys(USER)
+      await driver.findElement(passwordInput).sendKeys(PASSWORD)
+      await driver.findElement(loginButton).click()
+
+      await driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Copy Referral Link")]')), 30000)
+
+      console.log('-> Logged in! Waiting for open extension...')
+
+      // 截图登录状态
+      takeScreenshot(driver, 'login.png')
+
+      await driver.get(`chrome-extension://${extensionId}/popup.html`)
+
+      // 直到找到 "Status" 文本的 div 元素
+      await driver.wait(until.elementLocated(By.xpath('//div[contains(text(), "Status")]')), 30000)
+
+      await driver.sleep(5000)
+
+      console.log('-> Extension loaded!')
+
+      // if there is a page with a button "I got it", click it
+      try {
+        const gotItButton = await driver.findElement(By.xpath('//button[contains(text(), "I got it")]'))
+        await gotItButton.click()
+        console.log('-> "I got it" button clicked!')
+      } catch (error) {
+        // save rendered dom to file
+        const dom = await driver.findElement(By.css('html')).getAttribute('outerHTML')
+        fs.writeFileSync('dom.html', dom)
+        console.error('-> No "I got it" button found!')
+      }
+
+      // if found a div include text "Sorry, Gradient is not yet available in your region. ", then exit
+      try {
+        const notAvailable = await driver.findElement(By.xpath('//*[contains(text(), "Sorry, Gradient is not yet available in your region.")]'))
+        console.log('-> Sorry, Gradient is not yet available in your region. ')
+        await driver.quit()
+        process.exit(1)
+      } catch (error) {
+        console.log('-> Gradient is available in your region. ')
+      }
+
+      await driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Today\'s Taps")]')), 30000)
+
+      const statusBadge = await driver.findElement(By.css('.absolute.mt-3.right-0.z-10')).getText()
+
+      const dom = await driver.findElement(By.css('html')).getAttribute('outerHTML')
+      fs.writeFileSync('dom.html', dom)
+
+      await takeScreenshot(driver, 'status.png')
+
+      console.log('-> Status:', statusBadge)
+
+      if (statusBadge.includes('Disconnected')) {
+        console.log('-> Failed to connect! may be the proxy has been banned \nGenerating error report...')
+        await generateErrorReport(driver)
+        await driver.quit()
+        process.exit(1)
+      }
+
+      console.log('-> Connected! Starting API...')
+
+      // 截图链接状态
+      takeScreenshot(driver, 'connected.png')
+
+      const app = express()
+
+      app.get('/', async (req, res) => {
+        try {
+          // <div class="absolute mt-3 right-0 z-10">
+          const supportStatus = await driver.findElement(By.css('.absolute.mt-3.right-0.z-10')).getText()
+
+          // <div class="Helveticae w-1/2 flex justify-center items-center text-[14px] select-none cursor-pointer z-20 text-white">Reward</div>
+          const statusTab = await driver.findElement(By.xpath('//div[contains(text(), "Status")]'))
+          const rewardTab = await driver.findElement(By.xpath('//div[contains(text(), "Reward")]'))
+
+          // todaysTaps:
+          // <div class="Helveticae font-bold flex justify-center items-center select-none mt-[2px]" style="font-size: 24px;">0</div>
+          const todaysTaps = await driver.findElement(By.css('.mr-[8px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
+          const tadaysUptime = await driver.findElement(By.css('.mr-[4px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
+
+          // click on rewardTab tab
+          await rewardTab.click()
+
+          // todayReward:
+          // <div class="Helveticae font-bold flex justify-center items-center select-none mt-[2px]" style="font-size: 24px;">0</div>
+          const todayReward = await driver.findElement(By.css('.mr-[8px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
+          const seasonReward = await driver.findElement(By.css('.mr-[4px].border-theme-gray-border-2 > .font-bold.items-center')).getText()
+
+          const connected = badgeTexts.some(text => text.includes('Connected'))
+
+          res.json({
+            connected,
+            supportStatus: supportStatus,
+            today_taps: todaysTaps,
+            today_uptime: tadaysUptime,
+            today_reward: todayReward,
+            season_reward: seasonReward,
+          })
+        } catch (error) {
+          console.error(error)
+          await generateErrorReport(driver)
+          res.status(500).send('Internal Server Error')
+        }
+      })
+
+      app.listen(8090, '0.0.0.0', () => {
+        console.log('-> API server running on port 80 http://localhost:8090')
+      })
+
+      console.log('-> API started! Waiting for requests...')
+    } catch (error) {
+      console.error('Error occurred:', error)
+      if (driver) {
+        await generateErrorReport(driver)
+      }
+    } finally {
+      if (driver) {
+        await driver.quit()
+      }
+    }
+  })()
